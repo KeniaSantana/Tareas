169be60715512e0bf9a05e196138dd1a5ced7163
Kenia <23308060610371@cetis61.edu.mx>
2026-03-23 08:33:47 -0600

    x

--- tareas/src/main.py
+++ tareas/src/main.py
@@ -1,10 +1,5 @@
 import flet as ft
-from src.models.usuario import crear_tabla
 
-def main(page: ft.Page):
 
-    crear_tabla()
 
-    page.add(ft.Text("Todo funcionando 😎"))
-
-ft.app(target=main)
+ft.run(main)
